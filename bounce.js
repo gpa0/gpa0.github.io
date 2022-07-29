42c9510d8652cdc4db1f076381cd6665da5d1951
Gabriel Aguiar <67021784+gpa0@users.noreply.github.com>
2022-07-29 14:50:14 -0400

    Update bounce.js

--- bounce.js
+++ bounce.js
@@ -1,32 +1,34 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var ballRadius = 10;
-var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
+"use strict";
+
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+let ballRadius = 20;
+let x = canvas.width / 2;
+let y = canvas.height / 2;
+let dx = 2;
+let dy = 2;
 
 function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, 2 * Math.PI);
+  ctx.fillStyle = "#FF6CC0C";
+  ctx.fill();
+  ctx.closePath();
 }
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-        dx = -dx;
-    }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-        dy = -dy;
-    }
-    
-    x += dx;
-    y += dy;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  
+  if(x + ballRadius > canvas.width || x - ballRadius < 0) {
+    dx *= -1;
+  }
+  if(y + ballRadius > canvas.height || y - ballRadius < 0) {
+    dy *= -1;
+  }
+  
+  x += dx;
+  y += dy;
 }
 
 setInterval(draw, 10);
